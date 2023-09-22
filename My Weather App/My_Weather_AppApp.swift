82b36d411fe233a8fe8b206a89b145f9a838cc6f
Clement Ojiguo <clementojigs@hotmail.com>
2023-09-22 16:12:53 +0100

    Initial Commit

---
+++ My Weather App/My_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Weather_AppApp.swift
+//  My Weather App
+//
+//  Created by Clem OJ on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
